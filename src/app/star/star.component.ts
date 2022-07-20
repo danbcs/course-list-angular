6ebddd6c598d54362a5a912a140217a322c249e9
BR0056260263 - Daniel Bento de Castro <daniel.bcastro@enel.com>
2022-07-20 15:57:44 -0300

    Initial Commit from project course-list-angular

---
+++ src/app/star/star.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnChanges, SimpleChanges } from "@angular/core";
+import { startWith } from "rxjs";
+
+@Component( {
+    selector: 'app-star',
+    templateUrl: './star.component.html',
+    styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnChanges {
+
+    @Input()
+    rating: number = 0;
+
+    starWidth: number = 0;
+
+    ngOnChanges(): void {
+        this.starWidth = this.rating * 94 / 5;
+    }
+
+}
